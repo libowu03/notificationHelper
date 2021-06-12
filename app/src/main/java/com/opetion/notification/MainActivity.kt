e967eaf0a8a4f8fb872e961c3f14a2864fb89f36
libowu <1689829291@qq.com>
2021-06-12 22:46:17 +0800

    包名错误修改

--- app/src/main/java/com/opetion/notification/MainActivity.kt
+++ app/src/main/java/com/opetion/notification/MainActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.opetion.myapplication.R
 
 
 class MainActivity : AppCompatActivity() {
